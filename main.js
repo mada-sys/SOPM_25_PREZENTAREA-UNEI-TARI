e92c9fb18f6ccc1c036ce58aa6f3c2735e1dcbda
AntoniaValentina19 <115797885+AntoniaValentina19@users.noreply.github.com>
2025-10-20 23:51:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -26,3 +26,56 @@ const linkAction = () =>{
    navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== SWIPER HOME ===============*/
+const swiperHome = new Swiper('.home__swiper', {
+   loop: true,
+   slidesPerView: 'auto',
+   grabCursor: true,
+
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+
+   autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+   },
+})
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () =>{
+   const header = document.getElementById('header')
+   // Add a class if the bottom offset is greater than 50 of the viewport
+   this.scrollY >= 50 ? header.classList.add('bg-header') 
+                      : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+const swiperTestimonial = new Swiper('.testimonial__swiper', {
+   loop: true,
+   slidesPerView: 'auto',
+   spaceBetween: 48,
+   grabCursor: true,
+
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+
+   autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+   },
+})
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+   // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						     : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
